0ff5a1ea94bb2c9aa12eb65c369c00441410d271
jinwenwu <810270355@qq.com>
2021-03-23 18:23:31 +0800

    新增组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
-import { NativeModules } from 'react-native';
-
-type ZhoupuUiType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { ZhoupuUi } = NativeModules;
-
-export default ZhoupuUi as ZhoupuUiType;
+/*
+ * @LastEditors: jinwenwu
+ */
+export { default as ZPText } from './ZPText';
